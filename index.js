154be006ecedd329a36bb60bbfa2605d8e55fcab
KeithPetr <keith.petryshyn@gmail.com>
2023-03-21 16:10:33 -0400

    able to correct the text entered
    After clicking the process button, the text in the text area is now edited for mistakes and the new text takes it's place

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-import { apiKey } from './config.js'
-
-console.log(apiKey);
-const textAreaEl = document.getElementById("text-area");
-const processBtn = document.getElementById("process-btn")
+import { apiKey } from "./config.js";
+const get = (id) => document.getElementById(id)
+const textAreaEl = get("text-area");
+const processBtn = get("process-btn");
+const characterCountEl = get("character-count")
+const wordCountEl = get("word-count") 
 
 function editText() {
-  fetch("https://api.openai.com/v1/edits", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify({
-      model: "text-davinci-edit-001",
-      input: `${textAreaEl.value}`,
-      instruction: "Fix all of the grammar, punctuation and spelling mistakes",
-      n: 5,
-      temperature: 0.2,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+  if (textAreaEl.value) {
+    fetch("https://api.openai.com/v1/edits", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-edit-001",
+        input: `${textAreaEl.value}`,
+        instruction: "Fix all of the grammar, punctuation and spelling mistakes",
+        n: 1,
+        temperature: 0.2,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        textAreaEl.value = data.choices[0].text
+        countCharactersAndWords(textAreaEl.value);
+      })
+      .catch((error) => console.error(error));
+  }
 }
 
-processBtn.addEventListener('click', editText)
+function countCharactersAndWords(textarea) {
+  // Get the textarea value and remove spaces
+  console.log(textarea);
+  const textareaValue = textarea.replace(/\s/g, "");
+  console.log(textareaValue);
+
+  // Count the number of characters
+  const charCount = textareaValue.length;
+  console.log(charCount);
+
+  // Count the number of words
+  const wordCount = textarea
+    .split(/[\s,."'-]+/)
+    .filter((word) => word !== "").length;
+  console.log(wordCount);
+  // Return an object with the character and word counts
+  characterCountEl.textContent = charCount
+  wordCountEl.textContent = wordCount
+}
+
+
+
+processBtn.addEventListener("click", editText);
