60be594a5e257ae324444016aba8f1069a08ffac
KeithPetr <keith.petryshyn@gmail.com>
2023-03-23 09:14:39 -0400

    Adjust font-size and button height

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { apiKey } from "./config.js";
 
-const get = (id) => document.getElementById(id)
+const get = (id) => document.getElementById(id);
 const textAreaEl = get("text-area");
 const processBtn = get("process-btn");
-const characterCountEl = get("character-count")
-const wordCountEl = get("word-count")
-const resetBtn = get("reset-btn")
+const characterCountEl = get("character-count");
+const wordCountEl = get("word-count");
+const resetBtn = get("reset-btn");
 const copyBtn = get("copy-btn");
 const loadingSpinner = get("loading-spinner");
 
@@ -23,16 +23,17 @@ function editText() {
       body: JSON.stringify({
         model: "text-davinci-edit-001",
         input: `${textAreaEl.value}`,
-        instruction: "Fix all of the grammar, punctuation and spelling mistakes",
+        instruction:
+          "Fix all of the grammar, always add the correct punctuation and always correct spelling mistakes. Do not go to the next line after making corrections to the input text.",
         n: 1,
         temperature: 0.2,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
-        hideLoadingSpinner()
-        textAreaEl.value = data.choices[0].text
-        console.log(textAreaEl.value)
+        hideLoadingSpinner();
+        textAreaEl.value = data.choices[0].text;
+        console.log(textAreaEl.value);
         countCharactersAndWords(textAreaEl.value);
       })
       .catch((error) => console.error(error));
@@ -55,14 +56,14 @@ function countCharactersAndWords(textarea) {
     .filter((word) => word !== "").length;
   console.log(wordCount);
   // Return an object with the character and word counts
-  characterCountEl.textContent = charCount
-  wordCountEl.textContent = wordCount
+  characterCountEl.textContent = charCount;
+  wordCountEl.textContent = wordCount;
 }
 
 function reset() {
-  characterCountEl.textContent = '0';
-  wordCountEl.textContent = '0';
-  textAreaEl.value = '';
+  characterCountEl.textContent = "0";
+  wordCountEl.textContent = "0";
+  textAreaEl.value = "";
 }
 
 function showLoadingSpinner() {
@@ -76,7 +77,7 @@ function hideLoadingSpinner() {
 // ------------------ Event Listeners --------------------------
 
 processBtn.addEventListener("click", editText);
-resetBtn.addEventListener('click', reset)
-copyBtn.addEventListener("click", function() {
-  navigator.clipboard.writeText(textAreaEl.value)
+resetBtn.addEventListener("click", reset);
+copyBtn.addEventListener("click", function () {
+  navigator.clipboard.writeText(textAreaEl.value);
 });
